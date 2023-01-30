7df9573b4dd63605c0f8297b3d2a98ad6d601c70
ban-mazin <ban.qaddoori89@gmail.com>
2023-01-30 11:47:26 -0700

    add input and laber password

--- loginregisterapp/src/Login.jsx
+++ loginregisterapp/src/Login.jsx
@@ -3,6 +3,8 @@ export const Login = () => {
     <form>
       <label for="email">Email</label>
       <input type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+      <label for="password">Password</label>
+      <input tupe="password" placeholder="enter your password" id="password" name="password"/>
     </form>
   )
   
