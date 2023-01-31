10c694c1f333049535e9a51f1e0c00574dfd507c
ban-mazin <ban.qaddoori89@gmail.com>
2023-01-31 12:54:39 -0700

    add submit function

--- loginregisterapp/src/Login.jsx
+++ loginregisterapp/src/Login.jsx
@@ -5,14 +5,22 @@ export const Login = () => {
   const [email, setemail] = useState('');
   const [pass, setpass] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  }
+
   return (
-    <form>
-      <label for="email">Email</label>
-      <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+    <>
+      <form onSubmit={handleSubmit}>
+      <label for="email" >Email</label>
+      <input value={email} onChange={(e) => setemail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
       <label for="password">Password</label>
       <input value={pass} type="password" placeholder="******" id="password" name="password"/>
-      <button>Log in</button>
+      <button type="submit">Log in</button>
     </form>
+    </>
+    
   )
   
 }
