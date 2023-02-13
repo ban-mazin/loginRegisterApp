00044805b57165d5761c87d98546ef3a8f88d973
ban-mazin <ban.qaddoori89@gmail.com>
2023-02-13 11:39:25 -0700

    add register form

--- loginregisterapp/src/Register.jsx
+++ loginregisterapp/src/Register.jsx
@@ -1,8 +1,21 @@
 import React, {useState} from "react";
 
 export const Register = () => {
+ const [email, setEmail]= useState('');
+ const [pass, setPass] = useState('');
+ const [name, setName] = useState('');
+
   return (
-    <> Register </>
+    <> 
+    <form>
+      <label for="email">email</label>
+      <input value={email} onChange={(e) => setEmail(e.target.value)} type="email"/>
+      <label for="password">password</label>
+      <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="******" id="password" name="password"/>
+      <button type="submit"> Register</button>
+    </form>
+    
+    </>
   )
   
 }
