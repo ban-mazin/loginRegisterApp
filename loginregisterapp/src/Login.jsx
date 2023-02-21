76651599f009e20142504b6bcf2bf3cf3cc5a07f
ban-mazin <ban.qaddoori89@gmail.com>
2023-02-21 12:35:09 -0700

    set props on login

--- loginregisterapp/src/Login.jsx
+++ loginregisterapp/src/Login.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 
-export const Login = () => {
+export const Login = (props) => {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
 
@@ -19,7 +19,7 @@ export const Login = () => {
           <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="******" id="password" name="password"/>
           <button type="submit">Log In</button>
     </form>
-          <button> Already have an account? Register</button>
+          <button onClick={props.onFormSwitch}> Already have an account? Register</button>
     </>
     
   )
