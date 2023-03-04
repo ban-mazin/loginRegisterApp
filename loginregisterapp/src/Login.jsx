9ca570e42c59a9cd883704fb4e996fe0436b0927
ban-mazin <ban.qaddoori89@gmail.com>
2023-03-03 17:44:41 -0700

    fix bug

--- loginregisterapp/src/Login.jsx
+++ loginregisterapp/src/Login.jsx
@@ -17,7 +17,7 @@ export const Login = (props) => {
           <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
           <label for="password">Password</label>
           <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="******" id="password" name="password"/>
-          <button type="submit">Log In</button>
+          <button type="submit">LogIn</button>
     </form>
           <button onClick={props.onFormSwitch}> Already have an account?Register</button>
     </>
